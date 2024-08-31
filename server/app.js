ccff4d3ba67cd3be3d124a0912543c257845b52f
MuzamilEng <muzekhan777@gmail.com>
2024-08-31 21:03:34 +0500

    updated the server code

--- server/app.js
+++ server/app.js
@@ -14,7 +14,6 @@ const adminRoute = require("./routes/adminRoutes");
 const path = require("path");
 const errorHandler = require("./middleware/error-handler");
 const port = process.env.PORT || 5000;
-const axios = require("axios");
 
 
 connectDB();
@@ -54,44 +53,6 @@ const start = async () => {
   }
 };
 
-const WORDPRESS_API_URL = 'http://woocommerece-store.local/wp-json';
-const WOOCOMMERCE_CONSUMER_KEY = 'ck_8bfa874fbf2c1550274aabdaa8535af245d4c858';
-const WOOCOMMERCE_CONSUMER_SECRET = 'cs_24e678f3691aa3ab4b750ba214cfc9c2c80fe9fe';
-
-const fetchData = async (endpoint, config = {}) => {
-  try {
-      const response = await axios.get(`${WORDPRESS_API_URL}/${endpoint}`, {
-          ...config,
-          auth: {
-              username: WOOCOMMERCE_CONSUMER_KEY,
-              password: WOOCOMMERCE_CONSUMER_SECRET,
-          },
-          headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-          }
-      });
-      console.log('Data fetched successfully:', response.data);
-      return response.data;
-  } catch (error) {
-      if (error.response) {
-          // The request was made and the server responded with a status code
-          console.error('Error status:', error.response.status);
-          console.error('Error data:', error.response.data);
-      } else if (error.request) {
-          // The request was made but no response was received
-          console.error('No response received:', error.request);
-      } else {
-          // Something happened in setting up the request that triggered an Error
-          console.error('Error setting up request:', error.message);
-      }
-      throw error;
-  }
-};
-
-fetchData('wc/v3/products')
-  .then(data => console.log('Products:', data))
-  .catch(error => console.error('Error:', error));
 
 // Example usage:
 
